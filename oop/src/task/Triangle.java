76c505d699e0fd98fe968bcc4a027dc7248765c2
krishi karkal <krishi karkal>
2016-07-18 21:54:43 -0500

    triangle

---
+++ oop/src/task/Triangle.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Triangle {
+
+	 int a=10,b=20,r=10;
+	float area =(float) 0.5*(a*b);
+	int perimeter = a+b+r;
+	
+	float circle = (float) (3.14*r*r);
+	int diameter = 2*r;
+	
+	int rectanglearea = a*b;
+	int Perimeterrectangle = 2*a*b;
+	
+
+		
+	
+	
+}
---
+++ oop/src/task/Usage.java
@@ -0,0 +1,23 @@
+package task;
+
+import com.sun.javafx.geom.Area;
+
+public class Usage {
+
+	/**
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangle obj=new Triangle();
+		
+		System.out.println(" Area Of a triangle is "   + obj.area);
+		System.out.println(" Perimeter of triangle is "   +  obj.perimeter);
+		System.out.println(" Area of Circle is " + obj.circle);
+		System.out.println(" Diameter of a circle is " + obj.diameter);
+		System.out.println(" Area of Rectangle is " + obj.rectanglearea);
+		System.out.println(" Perimeter of a rectangle is " + obj.Perimeterrectangle);
+        
+
+	}
+
+}
